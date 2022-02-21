b4e48b947211488a3bd25f6c7aeecddf9d5cc61e
Aravinda89 <1513223865@qq.com>
2022-02-21 21:33:43 +0800

    struc people

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -6,6 +6,13 @@ contract SimpleStorage {
     // view
     uint256 favoriteNumber;
 
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // People public person = People({favoriteNumber:2, name: "Patrick"});
+
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
