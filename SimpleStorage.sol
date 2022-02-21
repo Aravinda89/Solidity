9b2cd0568720958f38b64b0e22d3a7937d6c2211
Aravinda89 <1513223865@qq.com>
2022-02-21 21:00:32 +0800

    pure, view keywords

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -3,13 +3,20 @@ pragma solidity ^0.6.0;
 contract SimpleStorage {
 
     // This will get initialized to 0
+    // view
     uint256 public favoriteNumber;
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
 
+    // view,
     function retrieve() public view returns(uint256) {
         return favoriteNumber;
     }
+
+    // // pure
+    // function retrieve(uint256 favoriteNumber) public pure {
+    //     favoriteNumber + favoriteNumber;
+    // }
 }
